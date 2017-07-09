96f60f23b60787efd7a33a369a628df9c82f8422
Varun Batra <codevarun@gmail.com>
2017-07-09 21:30:34 +0530

    Fix table horizontal separator bug.

--- table.go
+++ table.go
@@ -176,7 +176,7 @@ func (table *Table) Buffer() Buffer {
 		if table.Separator {
 			border := DefaultTxBuilder.Build(strings.Repeat("─", table.Width-2), table.FgColor, table.BgColor)
 			for i, cell := range border {
-				buffer.Set(i+1, pointerY+1, cell)
+				buffer.Set(table.innerArea.Min.X+i, pointerY+1, cell)
 			}
 		}
 	}
