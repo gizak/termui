d5109cf2e0f25fc499ae42922a5abacde7422ea8
Nouamane Tazi <nouamane98@gmail.com>
2021-01-27 23:17:44 +0100

    Add copyright comment to the example autoscroll.go

--- _examples/autoscroll.go
+++ _examples/autoscroll.go
@@ -1,3 +1,9 @@
+// Copyright 2017 Zack Guo <zack.y.guo@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT license that can
+// be found in the LICENSE file.
+
+// +build ignore
+
 package main
 
 import (
